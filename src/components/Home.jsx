4c16498dc7218f5511d6a1fbd4e4e70f1c89e8a2
Tuntunkr <tuntunkumar04051998@gmail.com>
2022-12-06 21:08:44 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Home.css"
+
+export default function Home() {
+  return (
+    <>
+    <section className="container-fluid">
+      <div className="conatiner">
+        <div className="Hero_heroContent">
+        <h1 className="title">Kilt</h1>
+          <p>A blockchain identity protocol for issuing<br/>
+          self-sovereign, anonymous, verifiable credentials and decentralized identifiers</p>
+
+          <div>
+          <a href="/articles" >READ MORE</a> 
+        </div>
+        </div>
+        
+      </div>
+    </section>
+    </>
+  );
+}
